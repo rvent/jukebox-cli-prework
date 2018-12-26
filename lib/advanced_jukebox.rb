95831c44c7936f48335b8f8f5a24c5de603e67fd
Rene Ventura <venturar729@gmail.com>
2018-12-26 23:54:38 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,13 +3,13 @@
 #with the correct path to this directory on your computer
 
 my_songs = {
-"Go Go GO" => 'home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/01.mp3',
-"LiberTeens" => 'home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/02.mp3',
-"Hamburg" =>  '/jukebox-cli-prework/audio/Emerald-Park/03.mp3',
-"Guiding Light" => 'home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/04.mp3',
-"Wolf" => 'home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/05.mp3',
-"Blue" => 'home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/06.mp3',
-"Graduation Failed" => 'home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/07.mp3'
+"Go Go GO" => 'audio/Emerald-Park/01.mp3',
+"LiberTeens" => 'audio/Emerald-Park/02.mp3',
+"Hamburg" =>  'audio/Emerald-Park/03.mp3',
+"Guiding Light" => 'audio/Emerald-Park/04.mp3',
+"Wolf" => 'audio/Emerald-Park/05.mp3',
+"Blue" => 'audio/Emerald-Park/06.mp3',
+"Graduation Failed" => 'audio/Emerald-Park/07.mp3'
 }
 
 def help
