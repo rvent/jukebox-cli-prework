14bf32b5609e3096c7f104d087a76d98f52f3de6
Rene Ventura <venturar729@gmail.com>
2018-12-26 21:38:03 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -31,8 +31,9 @@ end
 def play(songs)
   puts "Pleas enter a song name or number:"
   song_choice = gets.chomp
-  if songs.include?(song_choice) || (song_choice.to_i > 0 && song_choice.to_i < songs.length)
-    
-  end 
+  if songs.include?(song_choice) || ((song_choice.to_i > 0) && (song_choice.to_i < songs.length))
+    puts "Playing #{song_choice}"
+  else
+    puts "Invalid input, please try again"
   end
 end
