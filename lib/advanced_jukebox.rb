f79324fe440a4bc053c7c77e44644bfcb2bc36a9
Rene Ventura <venturar729@gmail.com>
2018-12-26 22:12:12 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -14,8 +14,15 @@ my_songs = {
 
 def help
   #this method should be the same as in jukebox.rb
-  
+  puts "I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+"
 end
+
+
 
 
 
