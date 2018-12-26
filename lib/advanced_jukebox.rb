1c9464770dc507dd469e294ab9bbad3fef547a72
Rene Ventura <venturar729@gmail.com>
2018-12-26 22:11:53 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,24 +3,24 @@
 #with the correct path to this directory on your computer
 
 my_songs = {
-"Go Go GO" => './jukebox-cli/audio/Emerald-Park/01.mp3',
-"LiberTeens" => './jukebox-cli/audio/Emerald-Park/02.mp3',
-"Hamburg" =>  '/jukebox-cli/audio/Emerald-Park/03.mp3',
-"Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-"Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-"Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-"Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+"Go Go GO" => './jukebox-cli-prework/audio/Emerald-Park/01.mp3',
+"LiberTeens" => './jukebox-cli-prework/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  '/jukebox-cli-prework/audio/Emerald-Park/03.mp3',
+"Guiding Light" => './jukebox-cli-prework/audio/Emerald-Park/04.mp3',
+"Wolf" => './jukebox-cli-prework/audio/Emerald-Park/05.mp3',
+"Blue" => './jukebox-cli-prework/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => './jukebox-cli-prework/audio/Emerald-Park/07.mp3'
 }
 
 def help
   #this method should be the same as in jukebox.rb
-
+  
 end
 
 
 
 def list(my_songs)
-  #this method is different! Collect the keys of the my_songs hash and 
+  #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
 end
 
@@ -33,7 +33,7 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
-  
+
 end
 
 def exit_jukebox
