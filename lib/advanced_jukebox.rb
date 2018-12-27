1028f0f2305e55c9096cdb4c87f4c6cfefabaf26
Rene Ventura <venturar729@gmail.com>
2018-12-27 00:18:52 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,13 +3,13 @@
 #with the correct path to this directory on your computer
 
 my_songs = {
-"Go Go GO" => 'audio/Emerald-Park/01.mp3',
-"LiberTeens" => 'audio/Emerald-Park/02.mp3',
-"Hamburg" =>  'audio/Emerald-Park/03.mp3',
-"Guiding Light" => 'audio/Emerald-Park/04.mp3',
-"Wolf" => 'audio/Emerald-Park/05.mp3',
-"Blue" => 'audio/Emerald-Park/06.mp3',
-"Graduation Failed" => 'audio/Emerald-Park/07.mp3'
+"Go Go GO" => '/home/rvent/.atom/.learn-ide/home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/01.mp3',
+"LiberTeens" => '/home/rvent/.atom/.learn-ide/home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  '/home/rvent/.atom/.learn-ide/home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/03.mp3',
+"Guiding Light" => '/home/rvent/.atom/.learn-ide/home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/04.mp3',
+"Wolf" => '/home/rvent/.atom/.learn-ide/home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/05.mp3',
+"Blue" => '/home/rvent/.atom/.learn-ide/home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => '/home/rvent/.atom/.learn-ide/home/restful-revert-5296/jukebox-cli-prework/audio/Emerald-Park/07.mp3'
 }
 
 def help
